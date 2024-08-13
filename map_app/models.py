78fcbe3b0b7e26f18bf9b884185faae3bb6aa9fe
lien <eneliviu@gmail.com>
2024-08-13 19:13:28 +0200

    Add location updates

--- map_app/models.py
+++ map_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Search(models.Model):
+    '''
+    Adress geocoding class
+    '''
+    address = models.CharField(max_length=100, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.address
+    
--- map_app/views.py
+++ map_app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import geocoder
 import folium
+from django.http import HttpResponse
 from folium.plugins import (Fullscreen, Draw, MousePosition)
+from .models import Search
+from .forms import SearchForm
+
 # Create your views here.
 
 
@@ -8,21 +13,43 @@ def index(request):
     '''
     Create folium Map object
     '''
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/map_app')
+    else:
+        form = SearchForm()
+    
+    address = Search.objects.all().last()  
+    location = geocoder.osm(address)
+    lat = location.lat
+    lng = location.lng
+    print([lat, lng])
+    country = location.country
+    if lat is None or lng is None:
+        address.delete()
+        return HttpResponse('Your address input is invalid')
+    
     m = folium.Map(tiles="cartodb positron",
                    zoom_start=3,
                    control_scale=True)
-    MousePosition().add_to(m)
     
-    draw = Draw(export=False, draw_options=True, position='bottomleft')
-    draw.add_to(m)
+    MousePosition().add_to(m)
+    Draw(export=False,
+         draw_options=True,
+         position='bottomleft').add_to(m)
     Fullscreen(position='topright').add_to(m)
      
-    # Get HTML representation of the Map() object
-    m = m._repr_html_()
+    folium.Marker([lat, lng],
+                  tooltip='Click for more',
+                  popup=country).add_to(m)
     
+    context = {'map': m._repr_html_(),  # HTML representation of the Map() object
+               'form': form}
     return render(
         request,
         'map_app/map.html',
-        {'map': m}
+        context
     )
 
