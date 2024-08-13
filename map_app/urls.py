b49f605495a56b6e90b8b3959197492d67e73e35
lien <eneliviu@gmail.com>
2024-08-13 18:03:15 +0200

    Add folium map

--- map_app/urls.py
+++ map_app/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from . import views as map_app_views
+
+urlpatterns = [
+    path('',
+         map_app_views.index,
+         name='map_app'),
+]
--- map_app/views.py
+++ map_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+import folium
+from folium.plugins import (Fullscreen, Draw, MousePosition)
 # Create your views here.
+
+
+def index(request):
+    '''
+    Create folium Map object
+    '''
+    m = folium.Map(tiles="cartodb positron",
+                   zoom_start=3,
+                   control_scale=True)
+    MousePosition().add_to(m)
+    
+    draw = Draw(export=False, draw_options=True, position='bottomleft')
+    draw.add_to(m)
+    Fullscreen(position='topright').add_to(m)
+     
+    # Get HTML representation of the Map() object
+    m = m._repr_html_()
+    
+    return render(
+        request,
+        'map_app/map.html',
+        {'map': m}
+    )
+
